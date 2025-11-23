b545767d47388d6d85ec1797768f32c585989fab
PolinaSold <clevnay@mail.ru>
2025-11-23 14:06:24 +0500

    Task5

--- Tyuiu.SoldatovaPA.Sprint2.Task5.V2.Lib/DataService.cs
+++ Tyuiu.SoldatovaPA.Sprint2.Task5.V2.Lib/DataService.cs
@@ -9,35 +9,34 @@ namespace Tyuiu.SoldatovaPA.Sprint2.Task5.V2.Lib
         {
             string season;
 
-            // Оператор switch для определения времени года
             switch (month)
             {
                 case 12:
                 case 1:
                 case 2:
-                    season = "Зима";
+                    season = "зима";  // с маленькой буквы
                     break;
 
                 case 3:
                 case 4:
                 case 5:
-                    season = "Весна";
+                    season = "весна";  // с маленькой буквы
                     break;
 
                 case 6:
                 case 7:
                 case 8:
-                    season = "Лето";
+                    season = "лето";  // с маленькой буквы
                     break;
 
                 case 9:
                 case 10:
                 case 11:
-                    season = "Осень";
+                    season = "осень";  // с маленькой буквы
                     break;
 
                 default:
-                    season = "Неизвестно";
+                    season = "неизвестно";  // с маленькой буквы
                     break;
             }
 
--- Tyuiu.SoldatovaPA.Sprint2.Task5.V2.Test/DataServiceTest.cs
+++ Tyuiu.SoldatovaPA.Sprint2.Task5.V2.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.SoldatovaPA.Sprint2.Task5.V2.Test
         {
             DataService ds = new DataService();
             int month = 1;
-            string wait = "Зима";
+            string wait = "зима";  // с маленькой буквы
             string res = ds.FindMonthSeason(month);
             Assert.AreEqual(wait, res);
         }
@@ -21,7 +21,7 @@ namespace Tyuiu.SoldatovaPA.Sprint2.Task5.V2.Test
         {
             DataService ds = new DataService();
             int month = 4;
-            string wait = "Весна";
+            string wait = "весна";  // с маленькой буквы
             string res = ds.FindMonthSeason(month);
             Assert.AreEqual(wait, res);
         }
@@ -31,7 +31,7 @@ namespace Tyuiu.SoldatovaPA.Sprint2.Task5.V2.Test
         {
             DataService ds = new DataService();
             int month = 7;
-            string wait = "Лето";
+            string wait = "лето";  // с маленькой буквы
             string res = ds.FindMonthSeason(month);
             Assert.AreEqual(wait, res);
         }
@@ -41,27 +41,7 @@ namespace Tyuiu.SoldatovaPA.Sprint2.Task5.V2.Test
         {
             DataService ds = new DataService();
             int month = 10;
-            string wait = "Осень";
-            string res = ds.FindMonthSeason(month);
-            Assert.AreEqual(wait, res);
-        }
-
-        [TestMethod]
-        public void ValidFindMonthSeason_December()
-        {
-            DataService ds = new DataService();
-            int month = 12;
-            string wait = "Зима";
-            string res = ds.FindMonthSeason(month);
-            Assert.AreEqual(wait, res);
-        }
-
-        [TestMethod]
-        public void ValidFindMonthSeason_February()
-        {
-            DataService ds = new DataService();
-            int month = 2;
-            string wait = "Зима";
+            string wait = "осень";  // с маленькой буквы
             string res = ds.FindMonthSeason(month);
             Assert.AreEqual(wait, res);
         }
